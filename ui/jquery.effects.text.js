0fb0a880a54f64c33d558e09c1d278e65cf4697b
Jesper Lindstroem Nielsen <Jesper@Webcomfort.dk>
2011-05-23 23:04:51 +0200

    Made initial steps for porting

--- ui/jquery.effects.text.js
+++ ui/jquery.effects.text.js
@@ -11,7 +11,138 @@
  *	jquery.effects.text.js
  */
 (function( $, undefined ) {
+	
+	function startTextAnim( o ) {
+	
+		return this.queue(
+	
+		function () {
+	
+			var replaceWith, tagReg, reg, html, i, $set, set, wordCount, duration, interval, parentCoords, $this = $(this); /* No height etc. */
+			$this.width($this.width());
+			$this.height($this.height());
+	
+	
+	/*
+					The following regular expression courtesy of Phil Haack
+					http://haacked.com/archive/2004/10/25/usingregularexpressionstomatchhtml.aspx
+				*/
+			tagReg = /(<\/?\w+((\s+\w+(\s*=\s*(?:".*?"|'.*?'|[^'">\s]+))?)+\s*|\s*)\/?>)/g;
+	
+			/* Translation: /(HTML tag plus spaces)|(word/letter without '<' plus spaces)/g */
+			if (o.words) {
+				reg = /(<\/?\w+((\s+\w+(\s*=\s*(?:".*?"|'.*?'|[^'">\s]+))?)+\s*|\s*)\/?>)\s*|([^\s<]+\s*)/g;
+			} else {
+				reg = /(<\/?\w+((\s+\w+(\s*=\s*(?:".*?"|'.*?'|[^'">\s]+))?)+\s*|\s*)\/?>)\s*|([^\s<]\s*)/g;
+			}
+	
+			/* Make sure the correct html is in place */
+			if (o.text !== '') {
+				$this.html(o.text);
+			}
+	
+			/* Set the current text to use */
+			o.text = $this.html();
+	
+			/* Get the words */
+			words = o.text.match(reg);
+	
+			/* Array for HTML, will join later */
+			html = [];
+	
+			/* Loop over the words and seperate them (put 'em in a span) */
+			for (i = 0, l = words.length; i < l; i++) {
+				var word = words[i];
+	
+				if (!word.match(tagReg)) {
+					html.push('<span>' + word + '</span>');
+				} else {
+					html.push(word);
+				}
+			}
+	
+	
+			/* See how many words there are */
+			wordCount = html.length;
+	
+			/* No words? halt */
+			if (!wordCount) {
+				return;
+			}
+	
+			/* Put the newer correct html in place */
+			$this.html(html.join(''));
+	
+			/* Retreive the total set of elements */
+			$set = $this.find('span:not(:has(span))');
+			set = $set.get();
+	
+			/* Calculate the duration and interval points */
+			interval = (o.duration / (1.5 * wordCount));
+	
+			duration = (o.duration - wordCount * interval);
+	
+			/* If the cycle needs to reverse, reverse it all */
+			if (o.reverse) {
+				set.reverse();
+			}
+	
+			/* Width, height, left, top of parent for calculations */
+			parentCoords = $.extend($this.offset(), {
+				width: $this.width(),
+				height: $this.height()
+			});
+	
+			/* Iterate over all the elements run their animation function on it */
+			for (i = 0, l = set.length; i < l; i++) {
+				var $word = $(set[i]);
+	
+				/* Do something to the element before the animation starts */
+				$.type(o.beforeAnimate) === 'function' && o.beforeAnimate.call($word);
+	
+	/*
+						Call the animation per element
+						This way each method can define it's manipulation per element
+					*/
+				o.animate.call($word, interval, duration, i, wordCount, parentCoords);
+			}
+	
+			setTimeout(
+	
+			function () { /* internal callback when event has finished, therefor pass object */
+				$.type(o.finished) === 'function' && o.finished.call($this);
+	
+				/* normal object, expecting domElement, so give it */
+				$.type(o.callback) === 'function' && o.callback.call($this[0]);
+	
+				/* dequeue the shizzle */
+				$this.dequeue();
+			}, o.duration);
+	
+		});
+	};
+	
+	$.effects.text = {
+		explode: function ( o ) {
+			
+		},
+		
+		type: function ( o ) {
+		},
+		
+		build: function ( o ) {
+			
+		},
+		
+		blockfade: function ( o ) {
+			
+		},
+		
+		
+	
+	}
 
+/** given from the ported version **/
 	
 	var defaultOptions = {
 		easing: 'linear',
